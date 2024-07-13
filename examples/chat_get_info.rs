4e249de2c3c006c0d0403213d55dc6343f4ad569
Andrei G <k05h31@gmail.com>
2024-07-14 02:00:15 +0300

    update chat_get_info

---
+++ examples/chat_get_info.rs
@@ -0,0 +1,54 @@
+#[macro_use]
+extern crate log;
+use vkteams_bot::{self, api::types::*};
+
+#[tokio::main]
+async fn main() {
+    // Load .env file
+    dotenvy::dotenv().expect("unable to load .env file");
+    // Initialize logger
+    pretty_env_logger::init();
+    info!("Starting...");
+    // Send message like text generation
+    let bot = Bot::default();
+    // Get chat_id from .env
+    let chat_id = ChatId(
+        std::env::var("VKTEAMS_CHAT_ID")
+            .expect("Unable to find VKTEAMS_CHAT_ID in .env file")
+            .to_string(),
+    );
+    // Bot action typing
+    bot.send_api_request(RequestChatsSendAction::new(
+        chat_id.to_owned(),
+        ChatActions::Typing,
+    ))
+    .await
+    .unwrap();
+    // Send message
+    match bot
+        .send_api_request(RequestChatsGetInfo::new(chat_id.to_owned()))
+        .await
+    {
+        Ok(res) => match res {
+            ResponseChatsGetInfo::Channel(chat) => {
+                info!("Channel: {:?}", chat.title.unwrap());
+            }
+            ResponseChatsGetInfo::Group(chat) => {
+                info!("Group: {:?}", chat.title.unwrap());
+            }
+            ResponseChatsGetInfo::Private(chat) => {
+                info!(
+                    "Private: {} {}",
+                    chat.first_name.unwrap(),
+                    chat.last_name.unwrap()
+                );
+            }
+            ResponseChatsGetInfo::None => {
+                debug!("None");
+            }
+        },
+        Err(e) => {
+            error!("Error: {}", e);
+        }
+    }
+}
--- src/api/chats/get_info.rs
+++ src/api/chats/get_info.rs
@@ -29,34 +29,34 @@ pub enum ResponseChatsGetInfo {
 #[derive(Serialize, Deserialize, Clone, Debug, Default)]
 #[serde(rename_all = "camelCase")]
 pub struct ResponseChatsPrivateGetInfo {
-    pub first_name: String,
-    pub last_name: String,
-    pub nick: String,
-    pub about: String,
-    pub is_bot: bool,
-    pub language: Languages,
+    pub first_name: Option<String>,
+    pub last_name: Option<String>,
+    pub nick: Option<String>,
+    pub about: Option<String>,
+    pub is_bot: Option<bool>,
+    pub language: Option<Languages>,
 }
 /// # Chats get info response method `chats/getInfo` for group chat
 #[derive(Serialize, Deserialize, Clone, Debug, Default)]
 #[serde(rename_all = "camelCase")]
 pub struct ResponseChatsGroupGetInfo {
-    pub title: String,
-    pub about: String,
-    pub rules: String,
-    pub invite_link: String,
-    pub public: bool,
-    pub join_moderation: bool,
+    pub title: Option<String>,
+    pub about: Option<String>,
+    pub rules: Option<String>,
+    pub invite_link: Option<String>,
+    pub public: Option<bool>,
+    pub join_moderation: Option<bool>,
 }
 /// # Chats get info response method `chats/getInfo` for channel chat
 #[derive(Serialize, Deserialize, Clone, Debug, Default)]
 #[serde(rename_all = "camelCase")]
 pub struct ResponseChatsChannelGetInfo {
-    pub title: String,
-    pub about: String,
-    pub rules: String,
-    pub invite_link: String,
-    pub public: bool,
-    pub join_moderation: bool,
+    pub title: Option<String>,
+    pub about: Option<String>,
+    pub rules: Option<String>,
+    pub invite_link: Option<String>,
+    pub public: Option<bool>,
+    pub join_moderation: Option<bool>,
 }
 impl BotRequest for RequestChatsGetInfo {
     const METHOD: &'static str = "chats/getInfo";
