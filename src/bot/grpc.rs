ceb075e4ad1f656b67e970b017c25d978ca9e4b7
Andrei G <k05h31@gmail.com>
2024-09-21 20:35:19 +0300

    fix docs

--- src/bot/grpc.rs
+++ src/bot/grpc.rs
@@ -3,7 +3,14 @@
 //!
 //! [supported in Kubernetes by default](https://kubernetes.io/docs/tasks/configure-pod-container/configure-liveness-readiness-startup-probes/#define-a-grpc-liveness-probe)
 //! ## Example
-//! pub async fn run_probe_app() -> Result<()> {
+//! ```
+//! extern crate tonic_health;
+//! extern crate tonic;
+//! extern crate anyhow;
+//! use crate::vkteams_bot::bot::net::shutdown_signal;
+//! 
+//! const DEFAULT_TCP_PORT: &str = "VKTEAMS_BOT_HTTP_PORT";
+//! pub async fn run_probe_app() -> anyhow::Result<()> {
 //!     // Create gRPC health reporter and service
 //!     let (_, health_service) = tonic_health::server::health_reporter();
 //!     // Get the port from the environment variable or use the default port 50555
@@ -12,13 +19,13 @@
 //!     tonic::transport::Server::builder()
 //!         .add_service(health_service)
 //!         .serve_with_shutdown(
-//!             format!("[::]:{tcp_port}").parse().unwrap(),
+//!             format!("[::1]:{tcp_port}").parse().unwrap(),
 //!             shutdown_signal(),
 //!         )
 //!         .await?;
 //!     Ok(())
 //! }
-//!
+//! ```
 use axum::Router;
 /// Inherit Router with gRPC probe
 pub trait GRPCRouter<S> {
