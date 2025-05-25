8e7ab7dee189cd89db3f3e7bc3832eb757d2f03a
Andrei G <k05h31@gmail.com>
2025-05-25 23:49:59 +0300

    fix type

--- crates/vkteams-bot-mcp/src/main.rs
+++ crates/vkteams-bot-mcp/src/main.rs
@@ -5,9 +5,11 @@ pub mod types;
 use rmcp::ServiceExt;
 use tokio::io::{stdin, stdout};
 use types::Server;
+use vkteams_bot::otlp::init;
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
+async fn main() -> anyhow::Result<(), Box<dyn std::error::Error>> {
+    let _guard = init()?;
     let server = Server::default();
     let transport = (stdin(), stdout());
     server.serve(transport).await?.waiting().await?;
--- crates/vkteams-bot-mcp/src/types.rs
+++ crates/vkteams-bot-mcp/src/types.rs
@@ -15,13 +15,9 @@ impl Server {
 
 impl Default for Server {
     fn default() -> Self {
-        let api_token =
-            std::env::var("VKTEAMS_BOT_API_TOKEN").expect("VKTEAMS_BOT_API_TOKEN is not set");
-        let api_url = std::env::var("VKTEAMS_BOT_API_URL").expect("VKTEAMS_BOT_API_URL is not set");
         let chat_id = std::env::var("VKTEAMS_BOT_CHAT_ID").expect("VKTEAMS_BOT_CHAT_ID is not set");
-        let bot = Bot::with_default_version(api_token, api_url).expect("Failed to create bot");
         Self {
-            bot: Arc::new(bot),
+            bot: Arc::new(Bot::default()),
             chat_id,
         }
     }
